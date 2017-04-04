964bbaa6c4a61e0408c2d354e7d579ea7d4192eb
Vipin Johney <vipin.johney@gmail.com>
2017-04-04 18:40:07 +0530

    Adding storyboard

--- StoryboardExtenstion.swift
+++ StoryboardExtenstion.swift
@@ -1,9 +1,34 @@
 //
-//  FileName.swift
+//  StoryboardExtenstion.swift
 //  iOSWorks
 //
 //  Created by Vipin Johney on 04/04/17.
-//  Copyright © 2017 ClaySys. All rights reserved.
+//  Copyright © 2017 VipinJohney. All rights reserved.
 //
+//  Use the code after understanding what it actually does
 
-import Foundation
+import UIKit
+
+// Create an object of a UIViewController using the identifier mentioned in storyboard
+// for the corresponding Controller
+//
+extension UIStoryboard {
+    
+    // This creates an object of the 'Main.storyboard' in the project bundle
+    class func mainStoryboard() -> UIStoryboard { return UIStoryboard(name: "Main", bundle: Bundle.main) }
+    
+    // This creates an object of the UIViewController inside the storyboard
+    // Usage::  let viewController = UIStoryboard.viewController()
+    //
+    class func viewController() -> UIViewController? {
+        return mainStoryboard().instantiateViewController(withIdentifier: "ViewControllerIdentifier")
+    }
+    
+    
+    // This creates an object of the UIViewController inside the storyboard and type casted to the controller class
+    // Usage::  let centerViewController: CenterViewController = UIStoryboard.centerViewController()
+    //
+    class func centerViewController() -> CenterViewController? {
+        return mainStoryboard().instantiateViewController(withIdentifier: "CenterViewController") as? CenterViewController
+    }
+}
